b758891a3a37f3d2aed965a7cc9cb5522e1cbab3
Artem Bondarenko <artem.arturovich.bondarenko@gmail.com>
2020-05-11 11:56:06 +0300

    Updates

---
+++ precache-manifest.87b535626f3c040ba3287f979a70547b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "32cde7ec2bc0d8d18920",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.e00ec3d3.chunk.css"
+  },
+  {
+    "revision": "32cde7ec2bc0d8d18920",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.28c75413.chunk.js"
+  },
+  {
+    "revision": "052338c15f0cfa810fb3",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.a33a0e9d.js"
+  },
+  {
+    "revision": "f2e9872f6300311f98e4",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.34237995.chunk.css"
+  },
+  {
+    "revision": "f2e9872f6300311f98e4",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.1e0d8dd3.chunk.js"
+  },
+  {
+    "revision": "ed00075375dcb45ca37b8766d7acb086",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  }
+];
